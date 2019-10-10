3e1111ce2d5347af72f0db6ae953a620835299bd
Allan Ricardo Pasquali <allan_pasquali@hotmail.com>
2019-10-10 13:57:37 -0300

    Commit

---
+++ EAD/EAD/EAD2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dolar d = 10.58;
+            // double valor = d;
+            // Console.WriteLine($"d {d.Valor}  e  valor {valor}");
+
+            Dolar d = (Dolar)10.58;
+            double valor = (double)d;
+            Console.WriteLine($"d {d.Valor}  e  valor {valor}") ;
+        }
+
+        public class Dolar
+        {
+            public double Valor { get; set; }
+
+            //public static implicit operator double(Dolar dolar)
+            //{
+            //    return dolar.Valor;
+            //}
+
+           // public static implicit operator Dolar(double valorRecebido)
+           // {
+           //     return new Dolar() { Valor = valorRecebido };
+           // }
+
+            public static explicit operator Dolar(double valorRecebido)
+            {
+                return new Dolar() { Valor = valorRecebido };
+            }
+
+            public static explicit operator double(Dolar dolar)
+            {
+                return dolar.Valor;
+            }
+        }
+    }
+}
---
+++ EAD/EAD/EAD5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAD5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaGolde saldo = new ContaGolde();
+            saldo.Sacar(300);
+            Console.ReadKey();
+        }
+
+        public class ContaBancaria
+        {
+            public double Saldo { get; private set; } // O private não deixa sobscrever o saldo -  no lugar de private poderia se protected 
+            public virtual void Sacar(double quantia)
+            {
+                if (quantia < 0)
+                {
+                    throw new Exception("Quantia insuficiente.");
+                }
+                Saldo -= quantia;
+            }
+        }
+
+        public class ContaGolde : ContaBancaria
+        {
+            public double Limite { get; protected set; }
+            public override void Sacar(double quantia)
+            {
+                if (quantia > Saldo)
+                {
+                    Limite = quantia - Saldo;
+                }
+                base.Sacar(quantia);
+            }
+        }
+
+        public class ContaUniversitaria : ContaBancaria
+        {
+            public override void Sacar(double quantia)
+            {
+                if (quantia > Saldo)
+                {
+                    throw new Exception("Saldo insuficiente");
+                }
+                base.Sacar(quantia);
+            }
+        }
+    }
+}
